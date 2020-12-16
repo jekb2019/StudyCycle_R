d7e71a82d747ff1ee3b85138ed4777e853ced47a
Jason Ko <jekb2019@gmail.com>
2020-12-16 21:25:35 +1300

    re-structure setting window component

--- src/components/setting_window/settingWindow.jsx
+++ src/components/setting_window/settingWindow.jsx
@@ -8,48 +8,44 @@ const SettingWindow = (props) => (
             <h1>Settings</h1>
             <span className={styles.close_button}>X</span>
         </div>
-        <div className={styles.setting_wrapper}>
-            <span className={styles.subject}>Focus Time</span>
-
-            <div className={styles.input_wrapper}>
-                <label>
-                    <input type="number" min="0" max="24" value="2"/>
-                    Hours
-                </label>
-                <label>
-                    <input type="number" min="0" max="59" value="0"/>
-                    Minutes
-                </label>
+        <div className={styles.components}>
+            <div className={styles.component}>
+                <div className={styles.component_header}></div>
+                <div>
+                    <label>
+                        <input className={styles.input_box} type="number" min="0" max="24" value="2"/>
+                        <span>Hours</span>
+                    </label>
+                    <label>
+                        <input className={styles.input_box} type="number" min="0" max="59" value="0"/>
+                        <span>Minutes</span>
+                    </label>
+                </div>
             </div>
-        </div>
-        <div className={styles.setting_wrapper}>
-            <span className={styles.subject}>Break Time</span>
-            <div className={styles.input_wrapper}>
-                <label>
-                    <input type="number" min="0" max="24" value="2"/>
-                    Hours
-                </label>
-                <label>
-                    <input type="number" min="0" max="59" value="0"/>
-                    Minutes
-                </label>
+            <div className={styles.component}>
+                <div className={styles.component_header}></div>
+                <div>
+                    <label>
+                        <input className={styles.input_box} type="number" min="0" max="24" value="2"/>
+                        <span>Hours</span>
+                    </label>
+                    <label>
+                        <input className={styles.input_box} type="number" min="0" max="59" value="0"/>
+                        <span>Minutes</span>
+                    </label>
+                </div>
             </div>
-        </div>
-        <div className={styles.setting_wrapper}>
-            <span className={styles.subject}>Goal Cycles</span>
-            <div className={styles.input_wrapper}>
-                <label>
-                    <input min="1" type="number" value="5"/>
-                    Cycles
-                </label>
-                <div className={styles.goal_set_button}>
-                    <span>Disable</span>
+            <div className={styles.component}>
+                <div className={styles.component_header}></div>
+                <div>
+                    <label>
+                        <input className={styles.input_box} min="1" type="number" value="5"/>
+                        <span>Cycles</span>
+                    </label>
                 </div>
             </div>
         </div>
-        <div className={styles.ok_button}>
-            <span>OK</span>
-        </div>
+        <button>OK</button>
     </div>
 );
 
