610d59f063aa724e038c9fbf88f7b2506a3716b6
Jason Ko <jekb2019@gmail.com>
2020-12-17 17:47:25 +1300

    implement basic ui for setting window

--- src/components/setting_window/settingWindow.jsx
+++ src/components/setting_window/settingWindow.jsx
@@ -5,13 +5,17 @@ const SettingWindow = (props) => (
     <div className={styles.window}>
         <div className={styles.header}>
             <img className={styles.logo} src="/images/small-logo.png" alt="small logo"/>
-            <h1>Settings</h1>
-            <span className={styles.close_button}>X</span>
+            <h1 className={styles.subject}>Settings</h1>
+            <div className={styles.close_button}>
+                <i className={`fas fa-times ${styles.close_icon}`}></i>
+            </div>
         </div>
         <div className={styles.components}>
             <div className={styles.component}>
-                <div className={styles.component_header}></div>
-                <div>
+                <div className={styles.component_header}>
+                    <span>Focus Time</span>
+                </div>
+                <div className={styles.inputs_wrapper}>
                     <label>
                         <input className={styles.input_box} type="number" min="0" max="24" value="2"/>
                         <span>Hours</span>
@@ -23,8 +27,10 @@ const SettingWindow = (props) => (
                 </div>
             </div>
             <div className={styles.component}>
-                <div className={styles.component_header}></div>
-                <div>
+                <div className={styles.component_header}>
+                    <span>Break Time</span>
+                </div>
+                <div className={styles.inputs_wrapper}>
                     <label>
                         <input className={styles.input_box} type="number" min="0" max="24" value="2"/>
                         <span>Hours</span>
@@ -36,8 +42,10 @@ const SettingWindow = (props) => (
                 </div>
             </div>
             <div className={styles.component}>
-                <div className={styles.component_header}></div>
-                <div>
+                <div className={styles.component_header}>
+                    <span>Goal Cycles</span>
+                </div>
+                <div className={styles.inputs_wrapper}>
                     <label>
                         <input className={styles.input_box} min="1" type="number" value="5"/>
                         <span>Cycles</span>
@@ -45,7 +53,7 @@ const SettingWindow = (props) => (
                 </div>
             </div>
         </div>
-        <button>OK</button>
+        <button className={styles.ok_button}>OK</button>
     </div>
 );
 
