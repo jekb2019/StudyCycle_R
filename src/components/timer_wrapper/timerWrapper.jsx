12dd3cf97c2160d349bb17bbddf8f42b1247afb0
Jason Ko <jekb2019@gmail.com>
2020-12-18 16:09:29 +1300

    Update timer wrapper UI

--- src/components/timer_wrapper/timerWrapper.jsx
+++ src/components/timer_wrapper/timerWrapper.jsx
@@ -11,7 +11,7 @@ const TimerWrapper = (props) => {
     return(
     <div className={styles.wrapper}>
         <div className={styles.settings}>
-            <span>2 hr 0 min &mdash; 0 hr 10 min &mdash; 5 Cycles</span>
+            <span><div className={styles.small_description}><span>Focus</span></div>2 hr 0 min <div className={styles.small_description}><span>Break</span></div>0 hr 10 min <div className={styles.small_description}><span>Cycles</span></div>5</span>
             <div className={styles.button} onClick={()=>{handleSettingClick(); props.handleClickSound();}}>
                 <i className={`fas fa-cog ${styles.icon}`} id="setting-icon"></i>
             </div>
