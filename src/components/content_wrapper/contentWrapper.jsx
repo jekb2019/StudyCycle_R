07446a34fa912f5faab13cbc4c4aa0d9d2792297
Jason Ko <jekb2019@gmail.com>
2021-01-09 23:10:27 +1300

    Refactor contentWrapper CSS

--- src/components/content_wrapper/contentWrapper.jsx
+++ src/components/content_wrapper/contentWrapper.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import TimerWrapper from '../timer_wrapper/timerWrapper';
 import SettingWindow from '../setting_window/settingWindow';
+import styles from './contentWrapper.module.css';
 
 const ContentWrapper = (props) => {
     return (
-        <div>
+        <div className={styles.content_wrapper}>
             <TimerWrapper/>
             <SettingWindow/>
             {/* {settingWindowOpen && 
