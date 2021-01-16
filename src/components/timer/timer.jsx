35040d734b81d548d40a1527e908bab6aee99a2b
Jason Ko <jekb2019@gmail.com>
2021-01-16 18:59:34 +1300

    Add color effect on goal reach for cycle indicator

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Clock from '../clock/clock';
 import Controller from '../controller/controller';
 import styles from './timer.module.css';
@@ -6,6 +6,7 @@ import styles from './timer.module.css';
 const Timer = (props) => {
     const focusIndicatorRef = useRef();
     const breakIndicatorRef = useRef();
+    const cycleIndicatorRef = useRef();
 
     const handleStartTimer = () => {
         props.handleStartTimer();
@@ -17,17 +18,29 @@ const Timer = (props) => {
         props.soundBox.playClickSound();
         props.handleResetTimer();
     }
+    useEffect(() => {
+        if(props.currentTimerStatus === 'focus') {
+            focusIndicatorRef.current.style.opacity = "1";
+            breakIndicatorRef.current.style.opacity = "0.3";
+        } else if(props.currentTimerStatus === 'break') {
+            focusIndicatorRef.current.style.opacity = "0.3";
+            breakIndicatorRef.current.style.opacity = "1";
+        } else if(props.currentTimerStatus === 'none') {
+            focusIndicatorRef.current.style.opacity = "0.3";
+            breakIndicatorRef.current.style.opacity = "0.3";
+        }
+    }, [props.currentTimerStatus]);
 
-    if(props.currentTimerStatus === 'focus') {
-        focusIndicatorRef.current.style.opacity = "1";
-        breakIndicatorRef.current.style.opacity = "0.3";
-    } else if(props.currentTimerStatus === 'break') {
-        focusIndicatorRef.current.style.opacity = "0.3";
-        breakIndicatorRef.current.style.opacity = "1";
-    } else if(props.currentTimerStatus === 'none') {
-        focusIndicatorRef.current.style.opacity = "0.3";
-        breakIndicatorRef.current.style.opacity = "0.3";
-    }
+    // Change cycle description UI color when goal is reached
+    
+    useEffect(() => {
+        if(props.isGoalCycleFinished) {
+            cycleIndicatorRef.current.style.color = "#DBA55D";
+        } else {
+            console.log(cycleIndicatorRef.current)
+            cycleIndicatorRef.current.style.color = "#808080";
+        }
+    }, [props.isGoalCycleFinished]);
 
     return(
         <div className={styles.timer}>
@@ -35,7 +48,7 @@ const Timer = (props) => {
                 <div ref={focusIndicatorRef} className={`${styles.indicator} ${styles.focusIndicator}`}>
                     <span>Focus</span>
                 </div>
-                <div className={styles.status}>
+                <div className={styles.status} ref={cycleIndicatorRef}>
                     <div>
                         <i className={`fas fa-biking ${styles.cycle_icon}`}></i>
                         <span className={styles.cycle}>{`${props.currentCycle}/${props.goalCycle}`}</span>
